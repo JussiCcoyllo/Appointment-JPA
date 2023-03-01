7606f8e1bcc68e419a0a9e15893e825fd2b2672e
Jussi Ccoyllo <jussi.ccoyllo@gmail.com>
2023-03-01 13:37:34 +0100

    Initial commit

---
+++ src/main/java/be/intecbrussel/model/Appointment.java
@@ -0,0 +1,15 @@
+package be.intecbrussel;
+
+public class Appointment {
+    public String id;
+    public String title;
+    public int day;
+    public int month;
+    public int year;
+    public int minute;
+    public int hour;
+
+
+
+
+}
